761a85e61e4bbe37d2678766bea5376564b5438b
Lucas Wakefield <lucas.wakefield@gmail.com>
2022-03-26 14:26:01 -0500

    created routes listnotes and createnote

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,28 @@
-const apiRouter = require('express').Router();
+const express = require('express');
+const { getNotes, createNote } = require('../db/notes');
+
+const apiRouter = express.Router();
+
+apiRouter.get('/listnotes', async (req, res, next) => {
+  try {
+    const notes = await getNotes();
+    res.send(notes);
+  } catch (error) {
+    next(error);
+  }
+});
+
+apiRouter.post('/createnote', async (req, res, next) => {
+  try {
+    const noteText = req.body;
+    const note = await createNote(noteText);
+    res.send({
+      success: true,
+      note,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = apiRouter;
