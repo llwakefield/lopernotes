e403c2fcb6ac28d2c851f8bcaabf0dcc72b89f9a
Lucas Wakefield <lucas.wakefield@gmail.com>
2022-03-26 14:33:12 -0500

    created get and create apiService

--- src/react/src/api/apiService.js
+++ src/react/src/api/apiService.js
@@ -1,1 +1,24 @@
+import axios from 'axios';
 
+const get = async () => {
+    const { data } = await axios.get('/api/listnotes');
+    return data;
+}
+
+const create = async (noteText) => {
+    const config = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(noteText),
+    }
+    const { data } = await axios.post('/api/createnote', config);
+    return data;
+}
+
+const apiService = {
+    get, create,
+}
+
+export default apiService;
